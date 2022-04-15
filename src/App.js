da637b94926b30b1eddf7e14aa989a100ba0b945
ddaniel9 <daniele009@live.it>
2022-04-15 02:38:24 +0200

    inserimento dei valori

--- src/App.js
+++ src/App.js
@@ -19,42 +19,12 @@ import {
   Text,
   useColorScheme,
   View,
-  Button
+  Button,
+  Alert ,
+  TextInput
 } from 'react-native';
 
-import {
-  Colors,
-  // DebugInstructions,
-  // Header,
-  // LearnMoreLinks,
-  // ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-// const Section = ({children, title}): Node => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
 
 const sports = [
   {
@@ -71,56 +41,120 @@ const sports = [
   },
 ];
 
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
 // const isDarkMode = useColorScheme() === 'dark';
  
 // const backgroundStyle = {
 //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
 // };
 export default class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     jwt: '', 
-  //     loading: true
-  //   }
-  // }
+  constructor() {
+    super();
+    this.state = {
+        dropdownValueME: -1,
+        dropdownValueNum: 'Nessuna',
+        dropdownValueCirc: 'Nessuna',
+        dropdownValueInterr: 'Nessuna',
+        myNumber:'6'
+    }
+  }
+
+  onChanged(text){
+    let newText = '';
+    let numbers = '0123456789';
+    for (var i=0; i < text.length; i++) {
+        if(numbers.indexOf(text[i]) > -1 ) {
+            newText = newText + text[i];
+        }
+        else {
+            // your call back function
+            alert("please enter numbers only");
+        }
+    }
+    if(parseInt(newText)<=30 || newText=='' ){this.setState({ myNumber: newText });}
+    else{alert("please enter numbers minore di 31");}
+    
+}
+
+
+  handleDropdownChange = (name) => (value) => {
+    if(name=='dropdownValueME'){
+      console.warn(name)
+    }
+    this.setState({ [name]: value });
+    console.log(value);
+ }
   render() { 
+    const { dropdownValueME,
+      myNumber,dropdownValueCirc,dropdownValueInterr,dropdownValueNum } = this.state
   return (
-    <SafeAreaView >
+    <SafeAreaView style={styles.container} >
       <StatusBar />
-      <ScrollView
+      {/* <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        >
-        {/* <Header /> */}
+        > */}
         <View >
             <DatePicker confirm={date => {console.warn(date)}}/>
         <Text>Massimo Edittale</Text>
-      <RNPickerSelect
-            onValueChange={(value) => console.log(value)}
-            items={[
-                { label: 'Scelgo io', value: 1 },
-                { label: 'Delitto', value: 'baseball' },
-                { label: 'Contravvenzione', value: 'hockey' },
-            ]}
-        />
+       
+              <RNPickerSelect style={styles.selectBoxMax}
+              // useNativeAndroidPickerStyle={false}
+                    items={[
+                        { label: 'Scelgo io', value: -1 },
+                        { label: 'Delitto', value: 6 },
+                        { label: 'Contravvenzione', value: 4 },
+                    ]}
+                    value={dropdownValueME}
+                    placeholder={{}}
+                    onValueChange={this.handleDropdownChange('dropdownValueME')}
+                />
+                 <View   style = {{flexDirection: 'row'}}>
+<TextInput 
+   style={styles.numberInput}
+   keyboardType='numeric'
+   onChangeText={(text)=> this.onChanged(text)}
+   value={myNumber}
+   maxLength={2}  //setting limit of input
+/>
+<Text style={{padding: 10}}> Anni di reato </Text>
+              {/* <RNPickerSelect style={styles.minSelectBox}
+                  value={dropdownValueNum}
+                  placeholder={{ label: "Select you favourite language", value: null }}
+                  items={[
+                      { label: 'Scelgo io', value: -1 },
+                      { label: 'Delitto', value: 6 },
+                      { label: 'Contravvenzione', value: 4 },
+                  ]}
+                  onValueChange={this.handleDropdownChange('dropdownValueNum')}
+              /> */}
+        </View>
+        <Separator />
          <Text>Circostanze</Text>
         <RNPickerSelect
-            onValueChange={(value) => console.log(value)}
+            value={dropdownValueCirc}
+            placeholder={{}}
             items={[
-                { label: 'Scelgo io', value: 1 },
-                { label: 'Delitto', value: 'baseball' },
-                { label: 'Contravvenzione', value: 'hockey' },
+                { label: '1/3', value: 0.33 },
+                { label: '1/2', value: 0.5 },
+                { label: '2/3', value: 0.66 },
+                { label: 'Nessuna', value: null },
             ]}
+            onValueChange={this.handleDropdownChange('dropdownValueCirc')}
         />
          <Text>Interruzione</Text>
         <RNPickerSelect
-            onValueChange={(value) => console.log(value)}
+            value={dropdownValueInterr}
+            placeholder={{label: "Nessuna", value: null}}
             items={[
-                { label: 'Scelgo io', value: 1 },
-                { label: 'Delitto', value: 'baseball' },
-                { label: 'Contravvenzione', value: 'hockey' },
+                { label: '1/4', value: 0.25 },
             ]}
+            onValueChange={this.handleDropdownChange('dropdownValueInterr')}
         />
+        <Separator />
+        <View style={styles.fixToText}>
         <Button
         title="Calcola"
         disabled
@@ -130,19 +164,16 @@ export default class App extends Component {
         color="#f194ff"
         onPress={() => Alert.alert('Button with adjusted color pressed')}
         />
+        </View>
           {/* <LearnMoreLinks /> */}
         </View>
-      </ScrollView>
+      {/* </ScrollView> */}
     </SafeAreaView>
   );
   }
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
@@ -154,6 +185,41 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  selectBoxMax:{
+  },
+  minSelectBox:{
+    marginLeft: 80, marginRight: 10
+  },
+  numberInput:{
+    // backgroundColor: '#C0C0C0',
+    // borderLeftWidth: 1,
+    // borderRightWidth: 1,
+    borderWidth :1,
+    height: 37,
+    width: 50,
+    // paddingLeft: 15,
+    marginLeft: 100,
+    borderStartWidth :2
+  },
+  separator: {
+    marginVertical: 8,
+    paddingHorizontal: 2,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
   },
 });
 
